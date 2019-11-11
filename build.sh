39758c7315388e70f222cbcade6397cac9151eca
Cüneyt Emre Yavuz <cuneytemreyavuz@gmail.com>
2019-11-11 21:15:17 +0300

    Styling changes on sidebar and topbar components

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 npm run build # Build the project
 ssh eusbolh@142.93.109.214 'cd /var/www/html; sudo rm -rf ./*;'
+ssh eusbolh@142.93.109.214 'sudo service nginx reload;'
 scp -r ~/Desktop/kunnect/dev/kunnect/build/* root@142.93.109.214:/var/www/html
--- src/components/topbar/Topbar.component.js
+++ src/components/topbar/Topbar.component.js
@@ -5,7 +5,24 @@ class Topbar extends Component {
   render() {
     return (
       <div className="knc-topbr-component">
-        Topbarr
+        <div className="knc-topbr-left-side">
+          search bar
+        </div>
+        <div className="knc-topbr-right-side">
+          <div className="knc-topbr-user-box-container">
+            <div className="knc-topbr-user-box">
+              <div className="knc-topbr-user-box-left-side">
+                <div className="knc-topbr-user-box-user-name">eusbolh</div>
+                <div className="knc-topbr-user-box-karma">20 karma</div>
+              </div>
+              <div className="knc-topbr-user-box-right-side">
+                <div className="knc-topbr-user-box-profile-image">
+                  i
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>      
       </div>
     );
   }
