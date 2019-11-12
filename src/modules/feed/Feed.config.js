3eec13b65d696188bb65d86158171b86da19c2af
Cüneyt Emre Yavuz <cuneytemreyavuz@gmail.com>
2019-11-12 16:33:11 +0300

    Feed module is added.

---
+++ src/modules/feed/Feed.config.js
@@ -0,0 +1,22 @@
+export const getPosts = () => [ // eslint-disable-line
+  {
+    id: 'adsa5d1sa35dsa31dasd',
+    type: 1,
+    post: {
+      title: 'Art.',
+      content: "ELI5: Why does a laptop say it's at 6-8% charge, and then it dies, but when it's at a higher charge, going from 60% to 59% takes a while?",
+      image: 'https://preview.redd.it/5ilxff64n5y31.png?width=640&crop=smart&auto=webp&s=8b412ce2b62b78adf74b971ed6e08a4ca3da4999',
+      comment_count: 14,
+      vote_count: 5914,
+      posted_at: '9 hours ago',
+    },
+    kuluster: {
+      name: 'explainlikeimfive',
+      image: 'https://b.thumbs.redditmedia.com/amUXWXfydWmcNl53gdZtOk9GSFoW5V7OJ-BMkvWedcc.png',
+    },
+    user: {
+      name: 'eusbolh',
+    },
+    vote: 1,
+  },
+];
